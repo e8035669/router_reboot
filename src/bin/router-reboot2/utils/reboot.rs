253574ca3b236b474e0b79b76bd578f42c7205c7
Yu-Ren Zhang <e8035669@gmail.com>
2022-11-16 21:42:36 +0800

    add reboot.rs

--- src/bin/router-reboot2/utils/reboot.rs
+++ src/bin/router-reboot2/utils/reboot.rs
@@ -1,6 +1,12 @@
 use serde::{Deserialize, Serialize};
 
+use crate::utils::Empty;
 use crate::utils::Value;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Default, Clone)]
+pub struct RebootEnvelop {
+    pub reboot_message: Empty,
+}
 
 #[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
 pub struct RebootResponse {
