e4a727e55c0da999a748826e60a960f87bcb6a6d
Yu-Ren Zhang <e8035669@gmail.com>
2022-11-21 22:38:48 +0800

    add clap command parse

--- src/bin/router-reboot2/main.rs
+++ src/bin/router-reboot2/main.rs
@@ -1,24 +1,43 @@
 mod utils;
 
+use clap::{Parser, Subcommand};
 use dotenv_codegen::dotenv;
 use std::error::Error;
 
 use utils::*;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let dlink_loginpw = dotenv!("DLINK_PASSWORD");
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    commands: Commands,
+}
 
+#[derive(Subcommand)]
+enum Commands {
+    Reboot,
+    Sms,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+
+    let dlink_loginpw = dotenv!("DLINK_PASSWORD");
     let mut dlink = DLinkRouter::new("http://192.168.1.254/DHMAPI/")?;
     dlink.login(dlink_loginpw)?;
     println!("Login success");
 
-    /*
-    let sms = dlink.get_sms()?;
-    println!("SMS:\n{}", sms);
-    */
-
-    dlink.reboot()?;
-    println!("Send reboot success");
+    match &cli.commands {
+        Commands::Reboot => {
+            dlink.reboot()?;
+            println!("Send reboot success");
+        }
+        Commands::Sms => {
+            let sms = dlink.get_sms()?;
+            println!("SMS:\n{}", sms);
+        }
+    }
 
     Ok(())
 }
